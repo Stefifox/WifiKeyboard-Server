e6b39815723c2dfe890f5fc25edb22b13a217914
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-06-07 22:30:50 +0200

    Added custom response by keys

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ app.get('/info', (req, res)=>{
 app.get('/key', (req, res)=>{
     let data = req.query
     let key = data.value
-
-    console.log(key)
+    //console.log(key)
     switch (key){
         case "mute":
             ks.sendKey('f13')
+            res.status(200).json({"status":"OK", "key":"f13", "name":"mute"})
             break
         case "audio":
             ks.sendKey('f14')
+            res.status(200).json({"status":"OK", "key":"f14", "name":"audio"})
+            break
+        default:
+            res.status(200).json({"status":"OK", "key":"none"})
             break
     }
 
-    res.status(200).json({"status":"OK"})
+
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
