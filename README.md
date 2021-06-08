4e9b5501613e9e8605a8ce14526acf735230d70d
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-06-08 12:03:26 +0200

    Added config file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Website](https://img.shields.io/website?down_color=grey&down_message=offline&style=for-the-badge&up_color=red&up_message=sefifoxapps.it&url=https%3A%2F%2Fstefifoxapps.it)
+![Website](https://img.shields.io/website?down_color=grey&down_message=offline&style=for-the-badge&up_color=red&up_message=sefifoxapps.it&url=https%3A%2F%2Fstefifoxapps.it) ![GitHub](https://img.shields.io/github/license/stefifox/WifiKeyboard-Server?style=for-the-badge)
 
 # WifiKeyboard-Server
 ## By Stefifox
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const ks = require('node-key-sender')
 const express = require('express')
 const path = require('path');
+const configs = require('./files/config.json')
 
 const app = express()
 const port = 3500;
+
+app.get('/connect', (req, res)=>{
+    res.status(200).json({"status":"OK", "connected":"true"})
+})
 
 app.get('/info', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, "/files/info.html"))
@@ -11,23 +16,15 @@ app.get('/info', (req, res)=>{
 
 app.get('/key', (req, res)=>{
     let data = req.query
-    let key = data.value
-    //console.log(key)
-    switch (key){
-        case "mute":
-            ks.sendKey('f13')
-            res.status(200).json({"status":"OK", "key":"f13", "name":"mute"})
-            break
-        case "audio":
-            ks.sendKey('f14')
-            res.status(200).json({"status":"OK", "key":"f14", "name":"audio"})
-            break
-        default:
-            res.status(200).json({"status":"OK", "key":"none"})
-            break
-    }
-
-
+    let id = data.id
+    console.log(id)
+    configs.buttons.forEach(e =>{
+        console.log(e)
+        if(e.id == id){
+            ks.sendKey(e.key)
+            res.status(200).json(e)
+        }
+    })
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
