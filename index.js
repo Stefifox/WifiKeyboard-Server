812a84dfe6eda727b66222f310a7bc5bdc440a8d
Stefano <30950154+Stefifox@users.noreply.github.com>
2021-06-12 15:23:02 +0200

    Added server_version adn version_code for app verification

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ const configs = require('./files/config.json')
 const app = express()
 const port = 3500;
 
+let version = "0.1.1"
+let versionCode = 2
+
 app.get('/connect', (req, res)=>{
-    res.status(200).json({"status":"OK", "connected":"true", "configs":configs})
+    res.status(200).json({"status":"OK", "connected":"true", "server_version": version, "version_code":versionCode ,"configs":configs})
+})
+
+app.get('/', (req, res)=>{
+    res.status(200).send("<h1>Home Page</h1><h3>Server Version " + version + "</h3>")
 })
 
 app.get('/info', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, "/files/info.html"))
+})
+
+app.get('/status', (req, res)=>{
+
 })
 
 app.get('/key', (req, res)=>{
